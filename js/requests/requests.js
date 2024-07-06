e62dc0240a543825b4081fce39dd6273c1a83877
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2024-07-05 21:26:50 -0400

    atualizando

--- js/requests/requests.js
+++ js/requests/requests.js
@@ -1,21 +1,3 @@
-// const removeElement = () => {
-//     let close = document.getElementsByClassName("close");
-//     // var table = document.getElementById('myTable');
-//     let i;
-//     for (i = 0; i < close.length; i++) {
-//         close[i].onclick = function () {
-//             let div = this.parentElement.parentElement;
-//             const nomeItem = div.getElementsByTagName('td')[0].innerHTML
-//             if (confirm("Você tem certeza?")) {
-//                 div.remove()
-//                 deleteItem(nomeItem)
-//                 alert("Removido!")
-//             }
-//         }
-//     }
-// }
-
-
 const insertButton = (parent) => {
     let span = document.createElement("span");
     let txt = document.createTextNode("\u00D7");
@@ -25,35 +7,75 @@ const insertButton = (parent) => {
 }
 
 
-const appendToList = (id, product, price) => {
-    let item = [id, product, price];
-    let table = document.getElementById("tbl-products")
-    let row = table.insertRow();
+const adicionarNaLista = (id, descricao, preco_custo, preco_venda, is_novo, data_criacao) => {
 
+    let table = document.getElementById("tbl-produtos")
+    let item = [id, descricao, preco_custo, preco_venda, is_novo, data_criacao];
+    let row = table.insertRow();
     for (let i = 0; i < item.length; i++) {
         let cel = row.insertCell(i);
         cel.textContent = item[i];
     }
-    insertButton(row.insertCell(-1))
-    // document.getElementById("newInput").value = "";
-    // document.getElementById("newQuantity").value = "";
-    // document.getElementById("newPrice").value = "";
 
-    // removeElement()
+
+    // Crie um novo elemento de botão
+    let btn = document.createElement('button');
+
+    // Defina o conteúdo do botão
+    btn.textContent = 'Excluir';
+
+    // Adicione um manipulador de eventos click para chamar a função desejada
+    btn.addEventListener('click', () => {
+        deletarProduto(id)
+    });
+
+    insertButton(row.insertCell(-1))
+
+    // Adicione o botão à última célula
+    let cel = row.insertCell();
+    cel.appendChild(btn);
 }
 
-const getProducts = async () => {
-    let url = 'http://127.0.0.1:5000/products';
+const getProdutos = async () => {
+    let url = 'http://127.0.0.1:5000/produto';
 
-    fetch(url, {method: 'GET'}).then((response) => response.json())
+    let table = document.getElementById("tbl-produtos");
+    // Delete rows of the table, except the header
+    while(table.rows.length > 1) {
+        table.deleteRow(1);
+    }
+
+    fetch(url, {method: 'GET'})
+        .then((response) => response.json())
         .then((data) => {
-            console.log('data: ', data)
-            data.forEach(item => appendToList(item.id, item.nome, item.valor))
+            console.log('data: ', data);
+            data['produtos'].forEach(item => adicionarNaLista(item.id, item.descricao, item.preco_custo, item.preco_venda, item.is_novo, item.data_criacao));
         })
         .catch((error) => {
             console.log(error);
         })
 }
+
+const deletarProduto = (id) => {
+    console.log(id)
+    let url = 'http://127.0.0.1:5000/produto?id=' + id;
+    fetch(url, {
+        method: 'delete'
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(() => {
+            atualizarTabela();  // atualizar a tabela após deletar o produto
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
 
 const cadastrarProduto = async () => {
     //teste imagem
@@ -78,4 +100,16 @@ const cadastrarProduto = async () => {
         .catch((error) => {
             console.log(error);
         })
+
+}
+
+const atualizarTabela = () => {
+    // Primeiro, limpe a tabela
+    let table = document.getElementById("tbl-produtos")
+    for (let i = table.rows.length - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+
+    // Então, readquira os dados e preencha a tabela
+    getProdutos();
 }
